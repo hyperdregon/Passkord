a2b2c3def444209d0adc1208681644602d849151
André Gonçalves <63752183+hyperdregon@users.noreply.github.com>
2021-11-20 20:33:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ do
     INCLUDES="$INCLUDES -I$h"
 done
 INCLUDES="-I$INCLUDES"
-gcc -DENC_KEY=$1 -DPASSWD=\"$2\" $INCLUDES $SOURCES -o passkord `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0` -rdynamic && sudo mv passkord /usr/bin
+gcc -DENC_KEY=$1 -DPASSWD=\"$2\" $INCLUDES $SOURCES -o passkord `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0` -rdynamic -fcommon && sudo mv passkord /usr/bin
 echo "Warning: You can delete all the contents of this folder, except the UI folder. That is where the UI files are, and without that the program won't load."
